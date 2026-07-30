01a38f399689649fac8100e2fe06ef8d502bf12e
Ayush Badoni <ayushbadoni5@gmail.com>
2026-07-30 13:03:57 +0530

    new line added

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,6 @@
 public class WelcomeToJava {
     public static void main(String[] args) {
+        System.out.println("Hello World");
         System.out.println("Welcome to the Java Track");
         System.out.println("I already know how to build programming logic");
         System.out.println("Now I am learnig how to express that logic using Java");
