9b6a677d941dababab95a3a5e0a1db5785da51e9
Ayush Badoni <ayushbadoni5@gmail.com>
2026-08-03 01:14:23 +0530

    shorted code

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -6,15 +6,6 @@ public class WelcomeToJava {
         System.out.println("Now I am learnig how to express that logic using Java");
         System.out.println("I will practice and improve");
         System.out.println("I will build projects and showcase my skills");
-        System.out.println("I am ready to learn and grow");
-        System.out.println("I am excited to start this journey");
-        System.out.println("I am confident that I will succeed");
-        System.out.println("I am grateful for this opportunity");
-        System.out.println("I am thankful to KodNest for providing this platform");
-        System.out.println("I am looking forward to the challenges ahead");
-        System.out.println("I am eager to contribute to the open source community");
-        System.out.println("I am passionate about technology and innovation");
-        System.out.println("I am committed to continuous learning and improvement");
-        System.out.println("I am ready to make a difference in the world");
+
     }
 }
