d6a7631b0772ed2402555472a2850e9d95e52343
Ayush Badoni <ayushbadoni5@gmail.com>
2026-08-06 12:15:08 +0530

    dowhileNested pushing

---
+++ track/M02/T01/ST01/dowhileNested.java
@@ -0,0 +1,17 @@
+package ST01;
+
+public class dowhileNested {
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            int j = 1;
+            do {
+                System.out.print(j);
+                j++;
+            } while (j <= i);
+            System.out.println();
+            i++;
+        } while (i <= 5);
+    }
+
+}
