8d53c40c39e96acd42676b0793ca972708d969ed
Ayush Badoni <ayushbadoni5@gmail.com>
2026-08-06 13:02:40 +0530

    labled loop

---
+++ track/M02/T01/ST01/P01/labledLoops.java
@@ -0,0 +1,12 @@
+public class labledLoops {
+    public static void main(String[] args) {
+        outer: for (int i = 1; i <= 3; i++) {
+            inner: for (int j = 0; j <= 4; j++) {
+                if (i == 2) {
+                    break outer;
+                }
+                System.out.println("i : " + i + "; j : " + j);
+            }
+        }
+    }
+}
