58ce4537b5eb0975d0489bf4b66a61eae577b740
Ayush Badoni <ayushbadoni5@gmail.com>
2026-08-12 11:59:09 +0530

    shadowing

---
+++ track/oops/classes/Shadowing/StudentApplication.java
@@ -0,0 +1,10 @@
+package track.oops.classes.Shadowing;
+
+public class StudentApplication {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.input("Raja", 19, 5.5);
+        s1.display();
+    }
+
+}
