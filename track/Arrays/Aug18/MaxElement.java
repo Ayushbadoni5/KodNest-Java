454dc706442a1802ab7d9c1fc509f5b532339d64
Ayush Badoni <ayushbadoni5@gmail.com>
2026-08-18 11:25:48 +0530

    max element

--- track/Arrays/Aug18/MaxElement.java
+++ track/Arrays/Aug18/MaxElement.java
@@ -1,5 +1,26 @@
 package track.Arrays.Aug18;
 
-public class MaxElement {
+import java.util.Scanner;
 
+public class MaxElement {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a[] = new int[5];
+        System.out.println("Enter the elements to find the maximum amoungst them: ");
+        for (int i = 0; i < a.length; i++) {
+            a[i] = scanner.nextInt();
+        }
+        System.out.println("Array Elements are: ");
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i] + " ");
+        }
+        int max = a[0];
+        for (int i = 0; i < a.length; i++) {
+            if (max < a[i]) {
+                max = a[i];
+            }
+        }
+        System.out.println();
+        System.out.println("Max element: " + max);
+    }
 }
