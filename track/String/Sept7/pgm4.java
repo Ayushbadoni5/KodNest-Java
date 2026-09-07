253a659448feedc6b9f239023589768fb0530cb5
Ayush Badoni <ayushbadoni5@gmail.com>
2026-09-07 17:25:32 +0530

    psm4 Strings

---
+++ track/String/Sept7/pgm4.java
@@ -0,0 +1,14 @@
+package track.String.Sept7;
+
+public class pgm4 {
+    public static void main(String[] args) {
+        String s3 = new String("Java");
+        String s4 = new String("Java");
+
+        if (s3.endsWith(s4)) {
+            System.out.println("Ref are Equal");
+        } else {
+            System.out.println("ref are not equal");
+        }
+    }
+}
