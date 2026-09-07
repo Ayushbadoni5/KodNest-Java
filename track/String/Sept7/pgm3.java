0308af27136cff99f1dd0966d867bf850564b50a
Ayush Badoni <ayushbadoni5@gmail.com>
2026-09-07 17:24:02 +0530

    Strings pgm3

---
+++ track/String/Sept7/pgm3.java
@@ -0,0 +1,14 @@
+package track.String.Sept7;
+
+public class pgm3 {
+    public static void main(String[] args) {
+        String s1 = "Java";
+        String s2 = "Java";
+
+        if (s1.equals(s2)) {
+            System.out.println("Ref are Equal");
+        } else {
+            System.out.println("ref are not equal");
+        }
+    }
+}
